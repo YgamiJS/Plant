1542378d2e23ba29b3e31d2b07b530902ed85615
Dima <dimasakharov@icloud.com>
2023-07-06 19:42:45 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
 import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Plant/",
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        interlaced: false,
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            active: false,
+            name: "removeEmptyAttrs",
+          },
+        ],
+      },
+    }),
+  ],
 });
